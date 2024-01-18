26e16b5d3aaef8a0571642745390fcdd64dbc7cc
killian.zhou@sepefrei.fr <GHvWrdt5TYeWyMJ>
2024-01-18 15:02:36 +0100

    die modification 🤷

--- weberp/sources/templates/userredirect.php
+++ weberp/sources/templates/userredirect.php
@@ -3,9 +3,12 @@ session_start();
 
 // if logout=success, redirect to login.php
 if (isset($_GET['logout']) && $_GET['logout'] == 'success') {
-    header("Location: ./login.php?logout=success");
+//    header("Location: ./login.php?logout=success");
+    die('<script>location.href="./login.php?logout=success";</script>');
 }
 
 if (!isset($_SESSION['username'])) {
-    header("Location: ./login.php");
+//    header("Location: ./login.php");
+    die('<script>location.href="./login.php";</script>');
 }
+
